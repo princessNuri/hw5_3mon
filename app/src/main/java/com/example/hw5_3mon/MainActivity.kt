a5fdabef073e8f993f3c3a4d429cfcf0414ebc1d
princessnuri <nurgaeva1606@gmail.com>
2022-07-26 19:00:30 +0600

    hh

--- app/src/main/java/com/example/hw5_3mon/MainActivity.kt
+++ app/src/main/java/com/example/hw5_3mon/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.btn.setOnClickListener {
             if (sum == 10 || binding.btn.text.equals("-1")) {
                 binding.btn.text = "-1"
